a3339fdba971dda282f26c1fdf3f9ad4acd666d4
DESKTOP-K8E61CL\User <ars_yangtw@yahoo.com>
2021-01-31 12:12:17 +0800

    modify mt5\fx_batch\exec_single_batch.bat

--- FX_BATCH/exec_single_batch.bat
+++ FX_BATCH/exec_single_batch.bat
@@ -1,7 +1,7 @@
 
 rem C:\Users\Arthur\Anaconda\python.exe P001_取得CME交易所資訊_盤後_每日市場成交資訊.py -t 20200622
-call C:\Users\Arthur\Documents\MT5\FX_BATCH\exec_single 20210125
-call C:\Users\Arthur\Documents\MT5\FX_BATCH\exec_single 20210126
+call C:\Users\Arthur\Documents\MT5\FX_BATCH\exec_single 20210130
+call C:\Users\Arthur\Documents\MT5\FX_BATCH\exec_single 20210131
 call C:\Users\Arthur\Documents\MT5\FX_BATCH\exec_single 20210127
 call C:\Users\Arthur\Documents\MT5\FX_BATCH\exec_single 20210128
 call C:\Users\Arthur\Documents\MT5\FX_BATCH\exec_single 20210129
